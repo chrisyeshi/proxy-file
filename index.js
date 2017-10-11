38266edbad7aaaf3ad6580b6974fee5b3e17b088
Chris Ye <chrisyeshi@gmail.com>
2017-10-11 15:42:19 -0700

    able to deserialize from a JSON file.

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const fs = require('fs');
 // const store = proxyStore.createStore('filename.json');
 
 const defaultFilePath = __dirname + 'proxyfile.json';
+const defaultTimeout = 0;
 
 const serialize = (content, filePath) => {
-	fs.writeFile(filePath, JSON.stringify(content), err => {
+	fs.writeFile(filePath, JSON.stringify(content, null, 2), err => {
 		if (err) throw err;
 	});
 };
 
 const createOnChangeSerializer = (inFilePath, inTimeout, inSerializeFunc) => {
 	const filePath = inFilePath ? inFilePath : defaultFilePath;
-	const timeout = inTimeout ? inTimeout : 1000;
-	const serializeFunc = inSerializeFunc ? inSerializeFunc : serailize;
+	const timeout = inTimeout !== undefined ? inTimeout : defaultTimeout;
+	const serializeFunc = inSerializeFunc ? inSerializeFunc : serialize;
 	let timer = null;
 	const resetTimeout = (timeout, callback) => {
 		if (timer) {
@@ -36,7 +37,10 @@ const createOnChangeSerializer = (inFilePath, inTimeout, inSerializeFunc) => {
 
 const proxyStore = {};
 proxyStore.createStore = (filePath, timeout, serializeFunc) => {
-	const content = {};
+	let content = {};
+	if (fs.existsSync(filePath)) {
+		content = JSON.parse(fs.readFileSync(filePath));
+	}
 	const onChangeSerialize =
 			createOnChangeSerializer(filePath, timeout, serializeFunc);
 	return new Proxy(content, onChangeSerialize);
--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 const proxyStore = require(__dirname + '/../index.js');
 const assert = require('assert');
+const fs = require('fs');
+
+const deltaTime = 10;
 
 describe('serialize', function() {
 	it('should serialize only once after frequent modifications',
 			function(done) {
 		let nSerialize = 0;
-		const timeout = 50;
+		const timeout = 0;
 		const store =
 				proxyStore.createStore(
 					undefined /* filePath */, timeout,
@@ -17,6 +20,30 @@ describe('serialize', function() {
 		setTimeout(() => {
 			assert.equal(1, nSerialize);
 			done();
-		}, timeout + 10);
+		}, timeout + deltaTime);
+	});
+
+	it('should write to file on object change', function(done) {
+		const timeout = 0;
+		const filePath = '/tmp/proxy-file.json';
+		const store = proxyStore.createStore(filePath, timeout);
+		const number = Math.floor(Math.random() * 1000);
+		store.number = number;
+		setTimeout(() => {
+			assert.equal(number, store.number);
+			fs.readFile(filePath, (err, data) => {
+				const content = JSON.parse(data);
+				assert.equal(number, content.number);
+				done();
+			});
+		}, timeout + deltaTime)
+	});
+});
+
+describe('deserialize', function() {
+	it('should load data from file if file exists', function(done) {
+		const store = proxyStore.createStore(__dirname + '/sample.json');
+		assert.equal('world', store.hello);
+		done();
 	});
 });
